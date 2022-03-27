8b407d4742ad9e31e594b2e64eb1e05d7afd0e26
Avish Kaarthik <avish@playo.co>
2022-03-27 20:49:35 +0530

    added basic pop and push on memory

--- main.cpp
+++ main.cpp
@@ -12,25 +12,70 @@ class stack {
         void pop();
 };
 
+void stack::push(){
+    int *temp;
+    if(base_ptr == NULL){
+        temp = new int;
+        cin>>*temp;
+        base_ptr = temp;
+        stack_ptr = base_ptr;
+        temp = NULL;
+        return;
+    }
+    
+    cout<< "\n stackptr after = > "<< stack_ptr;
+    stack_ptr = stack_ptr + 1;
+    temp = stack_ptr;
+    cin>>*temp;
+    cout<< "\n stackptr after = > "<< stack_ptr;
+    cout<< "\n stackptr data => "<<*stack_ptr;
+    cout<< "\n baseptr =>"<<base_ptr;
+    cout<< "\n basepte data =>"<<*base_ptr; 
+    cout<< "\n stack size => "<<stack_ptr - base_ptr;
+    temp = NULL;
+    return;
+}
+
+void stack::pop(){
+    int * temp;
+    if(base_ptr == NULL || stack_ptr == base_ptr){
+        cout<<"cannot pop";
+        return;
+    }
+    temp = stack_ptr;
+    cout<< " \n\n stack ptr after => "<<stack_ptr;
+    stack_ptr = temp -1;
+    temp = NULL;
+    cout<<"\n pop success";
+   
+    cout<< "\n\n stack ptr after => "<<stack_ptr;
+    cout<< "\n stack ptr data = > "<<*stack_ptr;
+    cout<<"\n base ptr => "<< base_ptr;
+    cout<< "\n basepte data =>"<<*base_ptr; 
+    cout<< "\n stack size => "<<stack_ptr - base_ptr;
+}
+
 int main(){
 
     int input = 1000;
-
+    stack s;
     while(input!=0){
         cin >> input;
         switch (input)
         {
         case 1:
-            cout<<"pushing stack";
+            s.push();
+            cout<<"\n\n pushing stack";
             break;
         case 2:
-            cout<<"popping stack";
+            s.pop();
+            cout<<" \n\n popping stack";
             break;
         case 0:
-            cout<<"exiting program";
+            cout<<"\n\nexiting program";
             break;
         default:
-            cout<<"invalid input";
+            cout<<" \n\ninvalid input";
             break;
         }
     }
